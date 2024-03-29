d7a3323df4bb28a60d9b27eca216cecd420afcdc
Yug Patel <ypatel@tribalscale.com>
2024-03-29 00:13:45 -0400

    Yug: the lighting control is now now working

--- app/src/main/java/com/example/smarthomedeviceapplication/MainActivity4.kt
+++ app/src/main/java/com/example/smarthomedeviceapplication/MainActivity4.kt
@@ -1,25 +1,53 @@
 package com.example.smarthomedeviceapplication
 
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity4 : ComponentActivity() {
+    private val databaseReference: DatabaseReference by lazy {
+        FirebaseDatabase.getInstance("https://smart-home-31620-default-rtdb.firebaseio.com").reference.child("control").child("blinds_open")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            setContentView(R.layout.activity_lighting_view)
+        setContentView(R.layout.activity_lighting_view)
 
+        val toggleButton = findViewById<ToggleButton>(R.id.toggleButton)
+
+        // Add ValueEventListener to update the toggle state based on Firebase value
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(Boolean::class.java) ?: false
+                toggleButton.isChecked = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity4, "Failed to read value.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Add OnClickListener to update Firebase value based on toggle state
+        toggleButton.setOnClickListener {
+            val newValue = toggleButton.isChecked
+            databaseReference.setValue(newValue)
         }
 
-
-
+        toggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            // isChecked is true if the toggle is in the "on" state, false otherwise
+            if (isChecked) {
+                // Do something when toggle is on
+                System.out.println("blinds are opening");
+            } else {
+                // Do something when toggle is off
+                System.out.println("blinds are closing");
+            }
+        }
     }
 }
-
-
-
-
-
-
-
