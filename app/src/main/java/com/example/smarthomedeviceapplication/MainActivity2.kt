cab7ce15097e50aaf0680d60e55c56ad65a39fb8
Lilarms <114709029+Lilarms@users.noreply.github.com>
2024-03-30 15:17:16 -0400

    yurrrr

--- app/src/main/java/com/example/smarthomedeviceapplication/MainActivity2.kt
+++ app/src/main/java/com/example/smarthomedeviceapplication/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.smarthomedeviceapplication
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.google.firebase.database.DataSnapshot
@@ -18,23 +19,71 @@ class MainActivity2 : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temperature_view)
 
-        val textView: TextView = findViewById(R.id.textView)
-        observeTemperature(textView)
+        val textViewCurrentTemp: TextView = findViewById(R.id.textViewCurrentTemp)
+        val textViewDesiredTemp: TextView = findViewById(R.id.textViewDesiredTemp)
+        val btnPlus: Button = findViewById(R.id.btnPlus)
+        val btnMinus: Button = findViewById(R.id.btnMinus)
+
+        observeCurrentTemperature(textViewCurrentTemp)
+        observeDesiredTemperature(textViewDesiredTemp)
+        setupButtonClickListeners(textViewDesiredTemp, btnPlus, btnMinus)
     }
 
-    private fun observeTemperature(textView: TextView) {
+    private fun observeDesiredTemperature(textViewDesiredTemp: TextView) {
+        val temperatureRef = databaseReference.child("control").child("desired_temp").child("value")
+
+        temperatureRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue(String::class.java)?.toIntOrNull() ?: 0
+                textViewDesiredTemp.text = "$value°C"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                textViewDesiredTemp.text = "Failed to fetch temperature"
+            }
+        })
+    }
+
+    private fun observeCurrentTemperature(textViewCurrentTemp: TextView) {
         val temperatureRef = databaseReference.child("sensors").child("temperature").child("value")
 
         temperatureRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.getValue(String::class.java)
-                textView.text = value ?: "N/A"
-                textView.text = "$value°C" // Adding "°C" at the end
+                val value = snapshot.getValue(String::class.java)?.toIntOrNull() ?: 0
+                textViewCurrentTemp.text = "$value°C"
             }
 
             override fun onCancelled(error: DatabaseError) {
-                textView.text = "Failed to fetch temperature"
+                textViewCurrentTemp.text = "Failed to fetch temperature"
             }
         })
+    }
+
+    private fun setupButtonClickListeners(textViewDesiredTemp: TextView, btnPlus: Button, btnMinus: Button) {
+        btnPlus.setOnClickListener {
+            incrementTemperature(textViewDesiredTemp)
+        }
+
+        btnMinus.setOnClickListener {
+            decrementTemperature(textViewDesiredTemp)
+        }
+    }
+
+    private fun incrementTemperature(textViewDesiredTemp: TextView) {
+        val currentTemperature = textViewDesiredTemp.text.toString().replace("°C", "").toInt()
+        val newTemperature = currentTemperature + 1
+        textViewDesiredTemp.text = "$newTemperature°C"
+        updateTemperatureInDatabase(newTemperature.toString())
+    }
+
+    private fun decrementTemperature(textViewDesiredTemp: TextView) {
+        val currentTemperature = textViewDesiredTemp.text.toString().replace("°C", "").toInt()
+        val newTemperature = currentTemperature - 1
+        textViewDesiredTemp.text = "$newTemperature°C"
+        updateTemperatureInDatabase(newTemperature.toString())
+    }
+
+    private fun updateTemperatureInDatabase(temperature: String) {
+        databaseReference.child("control").child("desired_temp").child("value").setValue(temperature)
     }
 }
--- app/src/main/java/com/example/smarthomedeviceapplication/MainActivity3.kt
+++ app/src/main/java/com/example/smarthomedeviceapplication/MainActivity3.kt
@@ -2,11 +2,9 @@ package com.example.smarthomedeviceapplication
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -18,43 +16,63 @@ class MainActivity3 : ComponentActivity() {
         FirebaseDatabase.getInstance("https://smart-home-31620-default-rtdb.firebaseio.com").reference
     }
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContent {
-            setContentView(R.layout.activity_irrigation_view)
-            val humidity = remember { mutableStateOf("Loading...") }
-            observeTemperature(humidity)
-           // textView = (TextView) findViewByID(text_temperature_details);
+        setContentView(R.layout.activity_irrigation_view)
 
-            val textView: TextView = findViewById(R.id.text_temperature_details);
-            textView.setText(humidity.value)
-            System.out.println("the temp is:" + humidity.value)
+        val irrigationToggleButton = findViewById<ToggleButton>(R.id.irrigation_automation_button)
+        val waterPumpToggleButton = findViewById<ToggleButton>(R.id.water_pump_on_button)
+        val humidityTextView = findViewById<TextView>(R.id.text_humidity)
 
-        }
-
-    }
-    private fun observeTemperature(temperature: MutableState<String>) {
-        val temperatureRef = databaseReference.child("sensors").child("temperature").child("value")
-
-        temperatureRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.getValue(String::class.java)
-                temperature.value = value ?: "N/A"
+        // Add ValueEventListener to update the toggle state based on Firebase value for irrigation
+        val irrigationReference = databaseReference.child("control").child("irrigation_active")
+        irrigationReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(Boolean::class.java) ?: false
+                irrigationToggleButton.isChecked = value
             }
 
             override fun onCancelled(error: DatabaseError) {
-                temperature.value = "Failed to fetch temperature"
+                Toast.makeText(this@MainActivity3, "Failed to read irrigation value.", Toast.LENGTH_SHORT).show()
             }
         })
-        System.out.println("Observed temp: $temperature" + temperature.value)
+
+        // Add ValueEventListener to update the toggle state based on Firebase value for water pump
+        val waterPumpReference = databaseReference.child("control").child("water_pump_on")
+        waterPumpReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(Boolean::class.java) ?: false
+                waterPumpToggleButton.isChecked = value
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity3, "Failed to read water pump value.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Add ValueEventListener to update the humidity value
+        val humidityReference = databaseReference.child("sensors").child("humidity").child("value")
+        humidityReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val value = dataSnapshot.getValue(String::class.java) ?: "N/A"
+                humidityTextView.text = "Current Humidity: $value"
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@MainActivity3, "Failed to read humidity value.", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        // Add OnClickListener to update Firebase value based on toggle state for irrigation
+        irrigationToggleButton.setOnClickListener {
+            val newValue = irrigationToggleButton.isChecked
+            irrigationReference.setValue(newValue)
+        }
+
+        // Add OnClickListener to update Firebase value based on toggle state for water pump
+        waterPumpToggleButton.setOnClickListener {
+            val newValue = waterPumpToggleButton.isChecked
+            waterPumpReference.setValue(newValue)
+        }
     }
 }
-
-
-
-
-
-
-
